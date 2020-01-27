8d3e477806859d75f09b4da83211854a370dc60a
Robert Price <59981666+rprice172@users.noreply.github.com>
2020-01-26 23:26:37 -0600

    weather formulas
    all work

--- BasicCalculatorLab/BasicCalculator/Program.cs
+++ BasicCalculatorLab/BasicCalculator/Program.cs
@@ -8,17 +8,30 @@ namespace BasicCalculator
         {
             Console.WriteLine("*** BASIC CALCULATOR ***");
 
-            Console.WriteLine("Enter the first number");
+            Console.WriteLine("Enter Current Temperature in Fahrenheit");
 
             // int.Parse will take a string data type and convert it to an int data type
-            int firstNumber = int.Parse(Console.ReadLine());
+            int Current = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Enter the second number");
+            Console.WriteLine("Enter Relative Humidity");
             int secondNumber = int.Parse(Console.ReadLine());
 
-            int sum = firstNumber + secondNumber;
+            int sum = Current - (9*(100 - secondNumber) / 25);
+            Console.WriteLine("The Dew Point is " + sum);
 
-            Console.WriteLine("The answer is " + sum);
+            Console.WriteLine("Enter Wind Speed");
+            int windSpeed = int.Parse(Console.ReadLine());
+
+            double windChill = 35.74
+            + (0.6125 * Current)
+           - 35.74 * Math.Pow(windSpeed, 0.16)
+           + 0.4275 * Current * Math.Pow(windSpeed, 0.16);
+
+            Console.WriteLine("The wind chill is" + windChill);
+
+       
         }
+
+        
     }
 }
